23c1de02294dc33ab0f039825304fc744f3f808c
TsunamiUmsterk <77205300+TsunamiUmsterk@users.noreply.github.com>
2021-08-28 12:23:59 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 var balance;
 var database;
-var enter50;
+var gameState = 2;
+var subtract;
+var decrease;
 
 function setup() {
 
@@ -8,7 +10,7 @@ function setup() {
 
   createCanvas(500, 500);
 
-  balance = Math.round(getBalance(), 2);
+  balance = getBalance();
 
   enter50 = createButton('50p');
   enter50.size(60, 50);
@@ -51,7 +53,49 @@ function setup() {
   enter5.style("font-size","20px");
   enter5.style("background-color","#66f542");
   enter5.mousePressed(increase5);
-  
+
+  dent50 = createButton('-50p');
+  dent50.size(60, 50);
+  dent50.position(223, 300);
+  dent50.style("font-size","20px");
+  dent50.style("background-color","#f54242");
+  dent50.mousePressed(decrease50);
+
+  dent1 = createButton('-1p');
+  dent1.size(60, 50);
+  dent1.position(113, 300);
+  dent1.style("font-size","20px");
+  dent1.style("background-color","#f54242");
+  dent1.mousePressed(decrease1);
+
+  dent1b = createButton('-£1');
+  dent1b.size(60, 50);
+  dent1b.position(278, 300);
+  dent1b.style("font-size","20px");
+  dent1b.style("background-color","#f54242");
+  dent1b.mousePressed(decrease1b);
+
+  dent10 = createButton('-10p');
+  dent10.size(60, 50);
+  dent10.position(168, 300);
+  dent10.style("font-size","20px");
+  dent10.style("background-color","#f54242");
+  dent10.mousePressed(decrease10);
+
+  dent2 = createButton('-£2');
+  dent2.size(60, 50);
+  dent2.position(333, 300);
+  dent2.style("font-size","20px");
+  dent2.style("background-color","#f54242");
+  dent2.mousePressed(decrease2);
+
+  dent5 = createButton('-£5');
+  dent5.size(60, 50);
+  dent5.position(388, 300);
+  dent5.style("font-size","20px");
+  dent5.style("background-color","#f54242");
+  dent5.mousePressed(decrease5);
+
 }
 
 function draw() {
@@ -87,6 +131,30 @@ function increase2() {
 
 function increase5() {
   updateBalance(5);
+}
+
+function decrease1() {
+  updateBalance(-0.01);
+}
+
+function decrease10() {
+  updateBalance(-0.1);
+}
+
+function decrease50() {
+  updateBalance(-0.5);
+}
+
+function decrease1b() {
+  updateBalance(-1);
+}
+
+function decrease2() {
+  updateBalance(-2);
+}
+
+function decrease5() {
+  updateBalance(-5);
 }
 
 function getBalance() {
