83d71e18587b734b63d7d3ffda7d3b3f3fa2e164
TsunamiUmsterk <77205300+TsunamiUmsterk@users.noreply.github.com>
2021-08-29 23:10:13 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,90 +8,90 @@ function setup() {
 
   database = firebase.database();
 
-  createCanvas(500, 500);
+  createCanvas(displayWidth, displayHeight/2);
 
   balance = getBalance();
 
   enter50 = createButton('50p');
-  enter50.size(60, 50);
-  enter50.position(223, 250);
+  enter50.size(displayWidth/6, displayHeight/6);
+  enter50.position(displayWidth/6*2, displayHeight/6);
   enter50.style("font-size","20px");
   enter50.style("background-color","#66f542");
   enter50.mousePressed(increase50);
 
   enter1 = createButton('1p');
-  enter1.size(60, 50);
-  enter1.position(113, 250);
+  enter1.size(displayWidth/6, displayHeight/6);
+  enter1.position(0, displayHeight/6);
   enter1.style("font-size","20px");
   enter1.style("background-color","#66f542");
   enter1.mousePressed(increase1);
 
   enter1b = createButton('£1');
-  enter1b.size(60, 50);
-  enter1b.position(278, 250);
+  enter1b.size(displayWidth/6, displayHeight/6);
+  enter1b.position(displayWidth/6*3, displayHeight/6);
   enter1b.style("font-size","20px");
   enter1b.style("background-color","#66f542");
   enter1b.mousePressed(increase1b);
 
   enter10 = createButton('10p');
-  enter10.size(60, 50);
-  enter10.position(168, 250);
+  enter10.size(displayWidth/6, displayHeight/6);
+  enter10.position(displayWidth/6, displayHeight/6);
   enter10.style("font-size","20px");
   enter10.style("background-color","#66f542");
   enter10.mousePressed(increase10);
 
   enter2 = createButton('£2');
-  enter2.size(60, 50);
-  enter2.position(333, 250);
+  enter2.size(displayWidth/6, displayHeight/6);
+  enter2.position(displayWidth/6*4, displayHeight/6);
   enter2.style("font-size","20px");
   enter2.style("background-color","#66f542");
   enter2.mousePressed(increase2);
 
   enter5 = createButton('£5');
-  enter5.size(60, 50);
-  enter5.position(388, 250);
+  enter5.size(displayWidth/6, displayHeight/6);
+  enter5.position(displayWidth/6*5, displayHeight/6);
   enter5.style("font-size","20px");
   enter5.style("background-color","#66f542");
   enter5.mousePressed(increase5);
 
   dent50 = createButton('-50p');
-  dent50.size(60, 50);
-  dent50.position(223, 300);
+  dent50.size(displayWidth/6, displayHeight/6);
+  dent50.position(displayWidth/6*2, displayHeight/3);
   dent50.style("font-size","20px");
   dent50.style("background-color","#f54242");
   dent50.mousePressed(decrease50);
 
   dent1 = createButton('-1p');
-  dent1.size(60, 50);
-  dent1.position(113, 300);
+  dent1.size(displayWidth/6, displayHeight/6);
+  dent1.position(0, displayHeight/3);
   dent1.style("font-size","20px");
   dent1.style("background-color","#f54242");
   dent1.mousePressed(decrease1);
 
   dent1b = createButton('-£1');
-  dent1b.size(60, 50);
-  dent1b.position(278, 300);
+  dent1b.size(displayWidth/6, displayHeight/6);
+  dent1b.position(displayWidth/6*3, displayHeight/3);
   dent1b.style("font-size","20px");
   dent1b.style("background-color","#f54242");
   dent1b.mousePressed(decrease1b);
 
   dent10 = createButton('-10p');
-  dent10.size(60, 50);
-  dent10.position(168, 300);
+  dent10.size(displayWidth/6, displayHeight/6);
+  dent10.position(displayWidth/6, displayHeight/3);
   dent10.style("font-size","20px");
   dent10.style("background-color","#f54242");
   dent10.mousePressed(decrease10);
 
   dent2 = createButton('-£2');
-  dent2.size(60, 50);
-  dent2.position(333, 300);
+  dent2.size(displayWidth/6, displayHeight/6);
+  dent2.position(displayWidth/6*4, displayHeight/3);
   dent2.style("font-size","20px");
   dent2.style("background-color","#f54242");
   dent2.mousePressed(decrease2);
 
   dent5 = createButton('-£5');
-  dent5.size(60, 50);
-  dent5.position(388, 300);
+  dent5.size(displayWidth/6, displayHeight/6);
+  dent5.position(displayWidth/6*5, displayHeight/3);
   dent5.style("font-size","20px");
   dent5.style("background-color","#f54242");
   dent5.mousePressed(decrease5);
@@ -103,22 +103,19 @@ function draw() {
 
   textSize(40);
   fill("orange");
-  text("Balance: £" + balance, 100, 100);
-
-
-  Math.round(balance, 2);
+  text("Balance: £" + balance, displayWidth/2-150, 100);
 }  
 
 function increase1() {
-  updateBalance(0.01);
+  updateBalance(1/100);
 }
 
 function increase10() {
-  updateBalance(0.1);
+  updateBalance(1/10);
 }
 
 function increase50() {
-  updateBalance(0.5);
+  updateBalance(1/2);
 }
 
 function increase1b() {
@@ -134,15 +131,15 @@ function increase5() {
 }
 
 function decrease1() {
-  updateBalance(-0.01);
+  updateBalance(-1/100);
 }
 
 function decrease10() {
-  updateBalance(-0.1);
+  updateBalance(-1/10);
 }
 
 function decrease50() {
-  updateBalance(-0.5);
+  updateBalance(-1/2);
 }
 
 function decrease1b() {
